8bb4dfc6e47abf7d0e53ab301c39ccdda86bfd90
FAJR <zeko85032@gmail.com>
2023-12-07 20:33:51 +0100

    ..

--- src/components/AddToCartButton.jsx
+++ src/components/AddToCartButton.jsx
@@ -4,8 +4,7 @@ function AddToCartButton({ onClick }) {
   return (
     <button
       onClick={onClick}
-      onTouchStart={handleTouchStart}
-      onTouchEnd={handleTouchEnd}
+
       className="px-4 py-1 m-1 text-[16px] transition ease-in duration-200 uppercase rounded-full hover:bg-blue-500 hover:border-blue-500 hover:text-white border border-gray-700 "
     >
       Add to cart
